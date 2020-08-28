ddbf786784dcbb309a8a45266a44be1ebfc128a8
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2020-08-29 01:00:00 +0600

    show all status

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article;
 
 class ArticleController extends Controller
 {
-    //
+    public function getAllArticle(){
+        $allArticles = Article::all();
+        return $allArticles;
+    }
 }
