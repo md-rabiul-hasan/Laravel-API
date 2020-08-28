c9a6d533a416ad0f8c65034ac15fa243c632a7b0
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2020-08-29 00:40:28 +0600

    all api route create

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,14 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/articles', 'ArticleController@getAllArticle');
+Route::get('/articles/{id}', 'ArticleController@getArticle');
+Route::post('/article', 'ArticleController@saveArticle');
+Route::put('/articles/{id}', 'ArticleController@updateArticle');
+Route::delete('/articles/{id}', 'ArticleController@deleteArticle');
